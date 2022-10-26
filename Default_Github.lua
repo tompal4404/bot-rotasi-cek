85fb57872ec43596864a279e0da48aa7ea635251
tompal4404 <116747755+tompal4404@users.noreply.github.com>
2022-10-26 23:25:41 +0700

    Add files via upload

---
+++ Default_Github.lua
@@ -0,0 +1,35 @@
+Rdp = {
+    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot3", Status = "Nonaktif"},
+    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
+    {nordp = 4, botname = "Bot4", Status = "Nonaktif"},
+    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
+    {nordp = 5, botname = "Bot5", Status = "Nonaktif"},
+    {nordp = 6, botname = "Bot6", Status = "Nonaktif"},
+    {nordp = 6, botname = "Bot6", Status = "Nonaktif"}
+}
+
+ShowIP = true
+ip_add = {
+    "192.168.0.1",
+    "192.168.0.2",
+    "192.168.0.3",
+    "192.168.0.4",
+    "192.168.0.5",
+    "192.168.0.6",
+    "192.168.0.7"
+}
+
+hookURL = "Hook URL Kalian"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
