2d181f4643668ad239d9d65cb4cc74c4e54d1fab
tompal4404 <116747755+tompal4404@users.noreply.github.com>
2022-10-27 11:12:10 +0700

    Update Default_Github.lua

--- Default_Github.lua
+++ Default_Github.lua
@@ -1,5 +1,5 @@
 Rdp = {
-    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 1, botname = "ipan123", Status = "Nonaktif"},
     {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
     {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
     {nordp = 2, botname = "Bot2", Status = "Nonaktif"},
